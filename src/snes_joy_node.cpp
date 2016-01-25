5cce22eb222991d8467a31a9e7ade02b1ecd0663
Matthew Bohl <mbohl@ucsd.edu>
2016-01-25 15:45:43 -0800

    Publishes motor movements to 'motor_control'.

--- src/snes_joy_node.cpp
+++ src/snes_joy_node.cpp
@@ -18,7 +18,7 @@ class snes_joy_handler {
 private:
     ros::NodeHandle nodeh;
     // This works in other code .. dont know why its not working here
-    //ros::NodeHandle pn("~");
+    //ros::NodeHandle pn("\~");
 
     ros::Subscriber joy_sub;
     ros::Publisher twist_pub, motor_pub;
@@ -31,7 +31,7 @@ public:
     snes_joy_handler() {
         joy_sub  = nodeh.subscribe<sensor_msgs::Joy>("/joy", 1, &snes_joy_handler::joy_receive, this);
         twist_pub = nodeh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
-        motor_pub = nodeh.advertise<std_msgs::String>("/arm_motor", 1);
+        motor_pub = nodeh.advertise<std_msgs::String>("/motor_control", 1);
 
         //pn.param<double>("max_ang_vel", max_ang_vel_, 1.0);
         //pn.param<double>("max_lin_vel", max_lin_vel_, 0.25);
@@ -52,6 +52,8 @@ public:
 
         twist.linear.x = max_lin_vel_*joy_msg->axes.at(1);
         twist.angular.z = max_ang_vel_*joy_msg->axes.at(0);
+		bool send_motor = false;
+		motor.data = "";
 
         // RT Indicates a movement in the positive
         // LT indicates a movement in the negative
@@ -61,30 +63,46 @@ public:
         if(joy_msg->buttons.at(RT) && !joy_msg->buttons.at(LT)) {
             if(joy_msg->buttons.at(A)) {
                 ROS_INFO("A going up");
+				send_motor = true;
+				motor.data = "0 10";
             }
             else if(joy_msg->buttons.at(B)) {
                 ROS_INFO("B going up");
+				send_motor = true;
+				motor.data = "1 10";	
             }
             else if(joy_msg->buttons.at(X)) {
                 ROS_INFO("X going up");
+				send_motor = true;
+				motor.data = "2 10";	
             }
             else if(joy_msg->buttons.at(Y)) {
                 ROS_INFO("Y going up");
+				send_motor = true;
+				motor.data = "3 10";	
             }
         }
 
         if(joy_msg->buttons.at(LT) && !joy_msg->buttons.at(RT)) {
             if(joy_msg->buttons.at(A)) {
                 ROS_INFO("A going down");
+				send_motor = true;
+				motor.data = "0 -10";	
             }
             else if(joy_msg->buttons.at(B)) {
                 ROS_INFO("B going down");
+				send_motor = true;
+				motor.data = "1 -10";	
             }
             else if(joy_msg->buttons.at(X)) {
                 ROS_INFO("X going down");
+				send_motor = true;
+				motor.data = "2 -10";	
             }
             else if(joy_msg->buttons.at(Y)) {
                 ROS_INFO("Y going down");
+				send_motor = true;
+				motor.data = "3 -10";	
             }
         }
             
@@ -96,8 +114,13 @@ public:
             ROS_INFO("START");
         }
 
-        twist_pub.publish(twist);
-    }
+	
+		if (send_motor == true) {
+			motor_pub.publish(motor);
+		}
+
+		twist_pub.publish(twist);
+	}
 
     ~snes_joy_handler() { }
 };
