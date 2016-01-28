95a175a18f465c84d43d5c7c69a58757e94b46a3
Aaron <aaron@Moscato.(none)>
2016-01-27 16:11:43 -0800

    Distance Detection

---
+++ src/getDistance.cpp
@@ -0,0 +1,87 @@
+
+#include <stdio.h>
+#include <ros/ros.h>
+#include "sensor_msgs/CameraInfo.h"
+#include "sensor_msgs/Image.h"
+#include "stereo_msgs/DisparityImage.h"
+#include "geometry_msgs/PoseWithCovarianceStamped.h"
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Pose.h>
+#include <tf/tf.h>
+#include <fstream>
+#include <math.h>
+#include <time.h> 
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <math.h> 
+
+
+
+
+// Keep track of Quadcopter state
+double T = 30; // ROS loop rate
+
+
+stereo_msgs::DisparityImage AaronIsCool;
+geometry_msgs::Pose ball;
+const int AVG_SIZE=50;
+float avg[AVG_SIZE];
+float finalAverage=0;
+
+float returnDistance(float measurement){
+
+
+	if (measurement<1){
+	finalAverage=0;
+		for (int i=AVG_SIZE-1;i>0;i--){
+			avg[i]=avg[i-1];
+			finalAverage=finalAverage+avg[i];
+		}
+	avg[0]=measurement;
+	finalAverage=finalAverage+avg[0];
+	finalAverage=finalAverage/AVG_SIZE;
+	}
+
+	return (finalAverage);
+
+}
+
+void distanceCB(const stereo_msgs::DisparityImage::ConstPtr& imagePtr)
+{
+	AaronIsCool.image.data=imagePtr->image.data;
+	AaronIsCool.f=imagePtr->f;
+	AaronIsCool.T=imagePtr->T;
+//	std::cout<<"Disparity: " << (int)AaronIsCool.image.data[(int) (240*640+320)*4+1]  << "\n";
+}
+
+void locateBall(const geometry_msgs::Pose::ConstPtr& posePtr)
+{
+
+	ball.position.z=returnDistance((AaronIsCool.f*AaronIsCool.T)/(int)AaronIsCool.image.data[(int) (posePtr->position.y*640+posePtr->position.x)*4+1]);
+//	std::cout<<"Ball Location : " << ball.position.z << "\n";
+
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "stereoDisparitytoDistance"); //Ros Initialize
+    ros::start();
+    ros::Rate loop_rate(T); //Set Ros frequency to 50/s (fast)
+    ros::NodeHandle n;
+
+
+	ros::Subscriber cameraImageLeft=n.subscribe<stereo_msgs::DisparityImage>("/stereo/disparity", 1, distanceCB);
+	ros::Subscriber receivedPoint=n.subscribe<geometry_msgs::Pose>("/ballLocation",1, locateBall);
+
+ros::Time stamp = ros::Time::now();
+AaronIsCool.image.data.resize(4*480*640);
+
+    while (ros::ok()) 
+    {
+	ros::spinOnce();
+	std::cout<<"Distance: " <<ball.position.z<< "\n";
+        loop_rate.sleep();
+    }
+}
+//END
+
