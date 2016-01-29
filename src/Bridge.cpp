b3eab9bed7a766504f3beb16c1d81d7e5f632e51
Matthew Bohl <mbohl@ucsd.edu>
2016-01-28 23:02:09 -0800

    Added pulling of motor parameters.  Added support for inversion of motors as well as moving in pre-defined increments for relative movement.

--- src/Bridge.cpp
+++ src/Bridge.cpp
@@ -12,9 +12,9 @@
 
 serial::Serial serial_port_;
 
-// Current motor status
+// Current/cached motor positions
 // Hope we dont have more than 10...
-int motors[10];
+std::map<std::string,int> motors[10];
 
 void movementCallback(const coconuts_common::ArmMovement::ConstPtr& msg){
 
@@ -32,23 +32,39 @@ void movementCallback(const coconuts_common::ArmMovement::ConstPtr& msg){
 
 			request << motor << " " << position << "|";
 
-			// Update our cached status 
-			motors[motor] = position;
+			// Update our cached position 
+			motors[motor]["position"] = position;
 		}
 
 	} else if ( msg->type == "RELATIVE" ) {
+
+		// Note actual requested values are ignored, intead, moves by 
+		// some default increments which depend on the motor in use
+		// Only the sign of the requested movement is used
 
 		for (int i = 0; i < msg->motor_positions.size(); i++) {
 			int motor, position;
 			motor = msg->motor_positions[i].motor;
 
 			// Add relative request to current motor position 
-			position = motors[motor] + msg->motor_positions[i].position;
+			if ( msg->motor_positions[i].position >= 0 ) {
+				if ( motors[motor]["inverted"] ) {
+					position = motors[motor]["position"] - motors[motor]["increment"];
+				} else {
+					position = motors[motor]["position"] + motors[motor]["increment"];
+				}
+			} else {
+				if ( motors[motor]["inverted"] ) {
+					position = motors[motor]["position"] + motors[motor]["increment"];
+				} else {
+					position = motors[motor]["position"] - motors[motor]["increment"];
+				}
+			}
 
 			request << motor << " " << position << "|";
 
 			// Update our cached status
-			motors[motor] = position;
+			motors[motor]["position"] = position;
 		}
 		
 	} else if ( msg->type == "POSE" ) {
@@ -75,17 +91,37 @@ int main(int argc, char **argv) {
 	ros::NodeHandle n;
 	ros::NodeHandle pn("~");
 	std::string port_name;
+	std::ostringstream param_name;
+	int motor_count;
 	
    	// Maybe reduce this so we dont buffer commands?
 	ros::Subscriber motor_sub = n.subscribe("motor_control",1, &movementCallback);
 	ros::Publisher arm_status_pub = n.advertise<coconuts_common::ArmStatus>("/arm_status",1);
     	
-   	ros::Rate loop_rate(1);
 
 	// Get Parameters
 	pn.param<std::string>("port", port_name, "/dev/ttyUSB0");
+	pn.param<int>("motor_count", motor_count, 4);
+
+	for(int i = 0; i < motor_count; i++) {
+
+		param_name.str("");
+		param_name.clear();
+		param_name << "motor_" << i << "_inverted";
+		pn.param<int>(param_name.str(), motors[i]["inverted"], 0);
+
+		param_name.str("");
+		param_name.clear();
+		param_name << "motor_" << i << "_increment";
+		pn.param<int>(param_name.str(), motors[i]["increment"], 10);
+
+		// Defaulting this until we read first status
+		motors[i]["position"] = 200;
+	}
 
 	// TODO Get Pre-fabs defined and included:)
+
+   	ros::Rate loop_rate(1);
 
 	// Open Serial port for Reading
 	try {
@@ -100,6 +136,7 @@ int main(int argc, char **argv) {
 	}
 
 	int count=0;
+
 	while(ros::ok()) {
 
 		coconuts_common::ArmStatus arm_status;
@@ -133,18 +170,18 @@ int main(int argc, char **argv) {
 						if ( (status.length() > 0) && (status.find(" ") >= 0) && (status.find("|") >= 0) ) {
 							motor_position.motor = atoi(status.substr(0, status.find(" ")).c_str());
 							motor_position.position = atoi(status.substr(status.find(" ") + 1, status.find("|")).c_str());
-							ROS_INFO("BEFORE: [%s].", status.c_str());
+							ROS_DEBUG("BEFORE: [%s].", status.c_str());
 							status = status.erase(0, status.find("|") + 1);
-							ROS_INFO("AFTER: [%s].", status.c_str());
+							ROS_DEBUG("AFTER: [%s].", status.c_str());
 							arm_status.motor_positions.push_back(motor_position);
 
 							// Copy values locally for relative movement
-							motors[motor_position.motor] = motor_position.position;
+							motors[motor_position.motor]["position"] = motor_position.position;
 
-							ROS_INFO("STATUS Found [%i %i|].", motor_position.motor, motor_position.position);
+							ROS_DEBUG("STATUS Found [%i %i|].", motor_position.motor, motor_position.position);
 						} else {
 							looking = false;
-							ROS_INFO("Done looking [%s].", status.c_str());
+							ROS_DEBUG("Done looking [%s].", status.c_str());
 						}
 						loops++;
 					}
