7b6ae6b53d62b92b7898c0b378c0f5fde9977516
Evan Gravelle <evangravelle@gmail.com>
2016-02-17 16:26:48 -0800

    fixing TF

--- src/turtlebot_tf.cpp
+++ src/turtlebot_tf.cpp
@@ -11,13 +11,13 @@ int main(int argc, char **argv) {
 	ros::NodeHandle nh;
    
 	tf2_ros::TransformBroadcaster tf_br;
-	geometry_msgs::TransformStamped left_camera, right_camera;
+	geometry_msgs::TransformStamped left_camera; // right_camera
 
 	nh.setParam("/baseline_length", baseline_length); 
 	nh.setParam("/camera_height", camera_height);
 	nh.setParam("/camera_from_center", camera_from_center);
 
-	ros::Rate rate(20.0);
+	ros::Rate rate(5.0);
 	while (ros::ok()){
 		left_camera.header.stamp = ros::Time::now();
 		left_camera.header.frame_id = "base_footprint";
@@ -30,19 +30,19 @@ int main(int argc, char **argv) {
 		left_camera.transform.rotation.z = 0;
 		left_camera.transform.rotation.w = 1;
 
-		right_camera.header.stamp = left_camera.header.stamp;
-		right_camera.header.frame_id = "base_footprint";
-		right_camera.child_frame_id = "right_camera";
-		right_camera.transform.translation.x = camera_from_center;
-		right_camera.transform.translation.y = -baseline_length/2.0;
-		right_camera.transform.translation.z = camera_height;
-		right_camera.transform.rotation.x = 0;
-		right_camera.transform.rotation.y = 0;
-		right_camera.transform.rotation.z = 0;
-		right_camera.transform.rotation.w = 1;
+		// right_camera.header.stamp = left_camera.header.stamp;
+		// right_camera.header.frame_id = "base_footprint";
+		// right_camera.child_frame_id = "right_camera";
+		// right_camera.transform.translation.x = camera_from_center;
+		// right_camera.transform.translation.y = -baseline_length/2.0;
+		// right_camera.transform.translation.z = camera_height;
+		// right_camera.transform.rotation.x = 0;
+		// right_camera.transform.rotation.y = 0;
+		// right_camera.transform.rotation.z = 0;
+		// right_camera.transform.rotation.w = 1;
 
 		tf_br.sendTransform(left_camera);
-		tf_br.sendTransform(right_camera);
+		// tf_br.sendTransform(right_camera);
 
 		rate.sleep();
 	}
