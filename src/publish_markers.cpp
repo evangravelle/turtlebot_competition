6b38ac3c9c45c07038ceab7fc1fdcb8a16a2a7f5
Evan Gravelle <evangravelle@gmail.com>
2016-02-19 18:58:14 -0800

    robot model basically finished, arm shown in rviz

--- src/publish_markers.cpp
+++ src/publish_markers.cpp
@@ -4,11 +4,13 @@
 #include <visualization_msgs/Marker.h>
 
 double ball_diameter = .0254; // in meters
-double arm_diameter = .0508;
-double arm_1_length = .0965;
-double arm_2_length = .0635;
-double arm_3_length = .0508;
-double claw_length = .0108;
+double arm_diameter = .03;
+double arm_1_length = .102;
+double arm_2_length = .076;
+double claw_length = .15;
+double camera_thickness = .04;
+double camera_width = .07;
+double camera_height = .05;
 
 int main(int argc, char **argv) {
 
@@ -16,16 +18,81 @@ int main(int argc, char **argv) {
 
 	ros::NodeHandle nh;
 
-	visualization_msgs::Marker ball_marker, arm_1_marker, arm_2_marker, arm_3_marker, claw_marker;
+	visualization_msgs::Marker ball_marker, arm_1_marker, arm_2_marker, claw_marker, camera_forward_marker, camera_up_marker, camera_down_marker;
+	//geometry_msgs::TransformStamped ball, arm_1, arm_2, claw;
 	tf2_ros::Buffer tf_buffer;
 	tf2_ros::TransformListener tf_listener(tf_buffer);
- 	geometry_msgs::TransformStamped ball;
- 	ros::Publisher marker_pub = nh.advertise<visualization_msgs::Marker>("/ball_marker", 10, true);
- 	ros::Publisher arm_1_pub = nh.advertise<visualization_msgs::Marker>("/arm_1_marker", 10, true);
- 	ros::Publisher arm_2_pub = nh.advertise<visualization_msgs::Marker>("/arm_2_marker", 10, true);
- 	ros::Publisher arm_3_pub = nh.advertise<visualization_msgs::Marker>("/arm_3_marker", 10, true);
- 	ros::Publisher claw_pub = nh.advertise<visualization_msgs::Marker>("/claw_marker", 10, true);
+ 	ros::Publisher marker_pub = nh.advertise<visualization_msgs::Marker>("/ball_marker", 1000, true);
+ 	ros::Publisher arm_1_pub = nh.advertise<visualization_msgs::Marker>("/arm_1_marker", 1000, true);
+ 	ros::Publisher arm_2_pub = nh.advertise<visualization_msgs::Marker>("/arm_2_marker", 1000, true);
+ 	ros::Publisher claw_pub = nh.advertise<visualization_msgs::Marker>("/claw_marker", 1000, true);
+ 	ros::Publisher camera_forward_pub = nh.advertise<visualization_msgs::Marker>("/camera_forward_marker", 1000, true);
+ 	ros::Publisher camera_up_pub = nh.advertise<visualization_msgs::Marker>("/camera_up_marker", 1000, true);
+  	ros::Publisher camera_down_pub = nh.advertise<visualization_msgs::Marker>("/camera_down_marker", 1000, true);
 	ros::Rate rate(10.0);
+
+	camera_forward_marker.header.frame_id = "camera_forward";
+	camera_forward_marker.header.stamp = ros::Time();
+	camera_forward_marker.id = 3;
+	camera_forward_marker.type = visualization_msgs::Marker::CUBE;
+	camera_forward_marker.action = visualization_msgs::Marker::ADD;
+	camera_forward_marker.pose.position.x = 0;
+	camera_forward_marker.pose.position.y = 0;
+	camera_forward_marker.pose.position.z = 0;
+	camera_forward_marker.pose.orientation.x = 0.0;
+	camera_forward_marker.pose.orientation.y = 0.0;
+	camera_forward_marker.pose.orientation.z = 0.0;
+	camera_forward_marker.pose.orientation.w = 1.0;
+	camera_forward_marker.scale.x = camera_thickness;
+	camera_forward_marker.scale.y = camera_width;
+	camera_forward_marker.scale.z = camera_height;
+	camera_forward_marker.color.a = 1.0; // Don't forget to set the alpha!
+	camera_forward_marker.color.r = 0.0;
+	camera_forward_marker.color.g = 0.0;
+	camera_forward_marker.color.b = 0.0;
+	camera_forward_marker.lifetime = ros::Duration();
+
+	camera_up_marker.header.frame_id = "camera_up";
+	camera_up_marker.header.stamp = ros::Time();
+	camera_up_marker.id = 3;
+	camera_up_marker.type = visualization_msgs::Marker::CUBE;
+	camera_up_marker.action = visualization_msgs::Marker::ADD;
+	camera_up_marker.pose.position.x = 0;
+	camera_up_marker.pose.position.y = 0;
+	camera_up_marker.pose.position.z = 0;
+	camera_up_marker.pose.orientation.x = 0.0;
+	camera_up_marker.pose.orientation.y = 0.0;
+	camera_up_marker.pose.orientation.z = 0.0;
+	camera_up_marker.pose.orientation.w = 1.0;
+	camera_up_marker.scale.x = camera_thickness;
+	camera_up_marker.scale.y = camera_width;
+	camera_up_marker.scale.z = camera_height;
+	camera_up_marker.color.a = 1.0; // Don't forget to set the alpha!
+	camera_up_marker.color.r = 0.0;
+	camera_up_marker.color.g = 0.0;
+	camera_up_marker.color.b = 0.0;
+	camera_up_marker.lifetime = ros::Duration();
+
+	camera_down_marker.header.frame_id = "camera_down";
+	camera_down_marker.header.stamp = ros::Time();
+	camera_down_marker.id = 3;
+	camera_down_marker.type = visualization_msgs::Marker::CUBE;
+	camera_down_marker.action = visualization_msgs::Marker::ADD;
+	camera_down_marker.pose.position.x = 0;
+	camera_down_marker.pose.position.y = 0;
+	camera_down_marker.pose.position.z = 0;
+	camera_down_marker.pose.orientation.x = 0.0;
+	camera_down_marker.pose.orientation.y = 0.0;
+	camera_down_marker.pose.orientation.z = 0.0;
+	camera_down_marker.pose.orientation.w = 1.0;
+	camera_down_marker.scale.x = camera_thickness;
+	camera_down_marker.scale.y = camera_width;
+	camera_down_marker.scale.z = camera_height;
+	camera_down_marker.color.a = 1.0; // Don't forget to set the alpha!
+	camera_down_marker.color.r = 0.0;
+	camera_down_marker.color.g = 0.0;
+	camera_down_marker.color.b = 0.0;
+	camera_down_marker.lifetime = ros::Duration();
 
 	ball_marker.header.frame_id = "ball";
 	ball_marker.header.stamp = ros::Time();
@@ -55,7 +122,7 @@ int main(int argc, char **argv) {
 	arm_1_marker.action = visualization_msgs::Marker::ADD;
 	arm_1_marker.pose.position.x = 0;
 	arm_1_marker.pose.position.y = 0;
-	arm_1_marker.pose.position.z = 0;
+	arm_1_marker.pose.position.z = arm_1_length/2;
 	arm_1_marker.pose.orientation.x = 0.0;
 	arm_1_marker.pose.orientation.y = 0.0;
 	arm_1_marker.pose.orientation.z = 0.0;
@@ -76,7 +143,7 @@ int main(int argc, char **argv) {
 	arm_2_marker.action = visualization_msgs::Marker::ADD;
 	arm_2_marker.pose.position.x = 0;
 	arm_2_marker.pose.position.y = 0;
-	arm_2_marker.pose.position.z = 0;
+	arm_2_marker.pose.position.z = arm_2_length/2;
 	arm_2_marker.pose.orientation.x = 0.0;
 	arm_2_marker.pose.orientation.y = 0.0;
 	arm_2_marker.pose.orientation.z = 0.0;
@@ -90,27 +157,6 @@ int main(int argc, char **argv) {
 	arm_2_marker.color.b = 0.0;
 	arm_2_marker.lifetime = ros::Duration();
 
-	arm_3_marker.header.frame_id = "arm_3";
-	arm_3_marker.header.stamp = ros::Time();
-	arm_3_marker.id = 3;
-	arm_3_marker.type = visualization_msgs::Marker::CYLINDER;
-	arm_3_marker.action = visualization_msgs::Marker::ADD;
-	arm_3_marker.pose.position.x = 0;
-	arm_3_marker.pose.position.y = 0;
-	arm_3_marker.pose.position.z = 0;
-	arm_3_marker.pose.orientation.x = 0.0;
-	arm_3_marker.pose.orientation.y = 0.0;
-	arm_3_marker.pose.orientation.z = 0.0;
-	arm_3_marker.pose.orientation.w = 1.0;
-	arm_3_marker.scale.x = arm_diameter;
-	arm_3_marker.scale.y = arm_diameter;
-	arm_3_marker.scale.z = arm_3_length;
-	arm_3_marker.color.a = 1.0; // Don't forget to set the alpha!
-	arm_3_marker.color.r = 1.0;
-	arm_3_marker.color.g = 1.0;
-	arm_3_marker.color.b = 0.0;
-	arm_3_marker.lifetime = ros::Duration();
-
 	claw_marker.header.frame_id = "claw";
 	claw_marker.header.stamp = ros::Time();
 	claw_marker.id = 3;
@@ -118,7 +164,7 @@ int main(int argc, char **argv) {
 	claw_marker.action = visualization_msgs::Marker::ADD;
 	claw_marker.pose.position.x = 0;
 	claw_marker.pose.position.y = 0;
-	claw_marker.pose.position.z = 0;
+	claw_marker.pose.position.z = claw_length/2;
 	claw_marker.pose.orientation.x = 0.0;
 	claw_marker.pose.orientation.y = 0.0;
 	claw_marker.pose.orientation.z = 0.0;
@@ -127,65 +173,31 @@ int main(int argc, char **argv) {
 	claw_marker.scale.y = arm_diameter;
 	claw_marker.scale.z = claw_length;
 	claw_marker.color.a = 1.0; // Don't forget to set the alpha!
-	claw_marker.color.r = 1.0;
-	claw_marker.color.g = 1.0;
+	claw_marker.color.r = 0.0;
+	claw_marker.color.g = 0.5;
 	claw_marker.color.b = 0.0;
 	claw_marker.lifetime = ros::Duration();
 
 	while(ros::ok()) {
-		try {
-        	ball = tf_buffer.lookupTransform("left_camera", "ball", ros::Time(0));
-    	}
-		catch (tf2::TransformException &ex) {
-        	ROS_WARN("%s",ex.what());
-        	ros::Duration(1.0).sleep();
-        	continue;
-    	}
 
-    	try {
-        	ball = tf_buffer.lookupTransform("arm_base", "arm_1", ros::Time(0));
-    	}
-		catch (tf2::TransformException &ex) {
-        	ROS_WARN("%s",ex.what());
-        	ros::Duration(1.0).sleep();
-        	continue;
-    	}
-
-    	try {
-        	ball = tf_buffer.lookupTransform("arm_1", "arm_2", ros::Time(0));
-    	}
-		catch (tf2::TransformException &ex) {
-        	ROS_WARN("%s",ex.what());
-        	ros::Duration(1.0).sleep();
-        	continue;
-    	}
-
-    	try {
-        	ball = tf_buffer.lookupTransform("arm_2", "arm_3", ros::Time(0));
-    	}
-		catch (tf2::TransformException &ex) {
-        	ROS_WARN("%s",ex.what());
-        	ros::Duration(1.0).sleep();
-        	continue;
-    	}
-
-    	try {
-        	ball = tf_buffer.lookupTransform("arm_3", "claw", ros::Time(0));
-    	}
-		catch (tf2::TransformException &ex) {
-        	ROS_WARN("%s",ex.what());
-        	ros::Duration(1.0).sleep();
-        	continue;
-    	}
-
-    	//marker.pose.position.x = ball.transform.translation.x;
-		//marker.pose.position.y = ball.transform.translation.y;
+		// try {
+        	// ball = tf_buffer.lookupTransform("camera_forward", "ball", ros::Time(0));
+        	//arm_1 = tf_buffer.lookupTransform("arm_1", "arm_2", ros::Time(0));
+        	//arm_2 = tf_buffer.lookupTransform("arm_2", "arm_claw", ros::Time(0));
+    	// }
+		// catch (tf2::TransformException &ex) {
+        //	 ROS_WARN("%s",ex.what());
+        //	 ros::Duration(0.02).sleep();
+        //	 continue;
+    	// }
 
 		marker_pub.publish(ball_marker);
 		arm_1_pub.publish(arm_1_marker);
 		arm_2_pub.publish(arm_2_marker);
-		arm_3_pub.publish(arm_3_marker);
 		claw_pub.publish(claw_marker);
+		camera_forward_pub.publish(camera_forward_marker);
+		camera_up_pub.publish(camera_up_marker);
+		camera_down_pub.publish(camera_down_marker);
 
     	rate.sleep();
 	}
--- src/publish_transforms.cpp
+++ src/publish_transforms.cpp
@@ -2,31 +2,31 @@
 #include <tf2_ros/transform_broadcaster.h>
 #include <coconuts_common/ArmStatus.h>
 #include <math.h>
+#include <tf2/LinearMath/Quaternion.h>
 
 double baseline_length = .248; // in inches
 double camera_height = .406; // in inches
 double camera_from_center = .038; // in y direction, in inches
-double arm_base_y = .197;
-double arm_base_z = .178;
-double arm_1_length = .0965;
-double arm_2_length = .0635;
-double arm_3_length = .0508;
+double arm_base_forward = .197;
+double arm_base_up = .178;
+double arm_diameter = .03;
+double arm_1_length = .102;
+double arm_2_length = .076;
+double claw_length = .15;
 
-geometry_msgs::TransformStamped camera_forward, arm_base, arm_1, arm_2, arm_3, claw;
+geometry_msgs::TransformStamped camera_forward, camera_up, camera_down, arm_base, arm_1, arm_2, claw;
 
 void armStatusCallback(const coconuts_common::ArmStatus::ConstPtr &arm_status) {
 
 	//static tf2_ros::TransformBroadcaster tf_br_static;
 
-	arm_base.transform.rotation.x = arm_status->motor_positions[0].position;
-	arm_1.transform.rotation.x = arm_status->motor_positions[1].position;;
-	arm_2.transform.rotation.x = arm_status->motor_positions[2].position;;
-	arm_3.transform.rotation.x = arm_status->motor_positions[3].position;;
-	claw.transform.rotation.x = arm_status->motor_positions[4].position;;
+	arm_base.transform.rotation.y = arm_status->motor_positions[0].position;
+	arm_1.transform.rotation.y = arm_status->motor_positions[1].position;;
+	arm_2.transform.rotation.y = arm_status->motor_positions[2].position;;
+	claw.transform.rotation.y = arm_status->motor_positions[3].position;;
 
 	//tf_br_static.sendTransform(arm_1);
 	//tf_br_static.sendTransform(arm_2);
-	//tf_br_static.sendTransform(arm_3);
 	//tf_br_static.sendTransform(claw);
 
 }
@@ -49,7 +49,7 @@ int main(int argc, char **argv) {
 	camera_forward.header.stamp = ros::Time::now();
 	camera_forward.header.frame_id = "base_footprint";
 	camera_forward.child_frame_id = "camera_forward";
-	camera_forward.transform.translation.x = camera_from_center;
+	camera_forward.transform.translation.x = -camera_from_center;
 	camera_forward.transform.translation.y = baseline_length/2.0;
 	camera_forward.transform.translation.z = camera_height;
 	camera_forward.transform.rotation.x = 0;
@@ -57,76 +57,99 @@ int main(int argc, char **argv) {
 	camera_forward.transform.rotation.z = 0;
 	camera_forward.transform.rotation.w = 1;
 
+	tf2::Quaternion camera_up_quat;
+	camera_up_quat.setRPY(0, -M_PI/2, 0);
+	camera_up.header.stamp = ros::Time::now();
+	camera_up.header.frame_id = "base_footprint";
+	camera_up.child_frame_id = "camera_up";
+	camera_up.transform.translation.x = -camera_from_center;
+	camera_up.transform.translation.y = -baseline_length/2.0;
+	camera_up.transform.translation.z = camera_height;
+	camera_up.transform.rotation.x = camera_up_quat.x();
+	camera_up.transform.rotation.y = camera_up_quat.y();
+	camera_up.transform.rotation.z = camera_up_quat.z();
+	camera_up.transform.rotation.w = camera_up_quat.w();
+
+	tf2::Quaternion camera_down_quat;
+	camera_down_quat.setRPY(0, -M_PI/2.0, 0);
+	camera_down.header.stamp = ros::Time::now();
+	camera_down.header.frame_id = "arm_2";
+	camera_down.child_frame_id = "camera_down";
+	camera_down.transform.translation.x = -arm_diameter;
+	camera_down.transform.translation.y = 0;
+	camera_down.transform.translation.z = arm_2_length/2.0;
+	camera_down.transform.rotation.x = camera_down_quat.x();
+	camera_down.transform.rotation.y = camera_down_quat.y();
+	camera_down.transform.rotation.z = camera_down_quat.z();
+	camera_down.transform.rotation.w = camera_down_quat.w();
+
 	arm_base.header.stamp = ros::Time::now();
 	arm_base.header.frame_id = "base_footprint";
 	arm_base.child_frame_id = "arm_base";
-	arm_base.transform.translation.x = 0;
-	arm_base.transform.translation.y = arm_base_y;
-	arm_base.transform.translation.z = arm_base_z;
+	arm_base.transform.translation.x = arm_base_forward;
+	arm_base.transform.translation.y = 0;
+	arm_base.transform.translation.z = arm_base_up;
 	arm_base.transform.rotation.x = 0;
 	arm_base.transform.rotation.y = 0;
 	arm_base.transform.rotation.z = 0;
 	arm_base.transform.rotation.w = 1;
 
+	tf2::Quaternion arm_1_quat;
+	arm_1_quat.setRPY(0, M_PI/8.0, 0);
 	arm_1.header.stamp = ros::Time::now();
 	arm_1.header.frame_id = "arm_base";
 	arm_1.child_frame_id = "arm_1";
 	arm_1.transform.translation.x = 0;
 	arm_1.transform.translation.y = 0;
 	arm_1.transform.translation.z = 0;
-	arm_1.transform.rotation.x = M_PI/4.0;
-	arm_1.transform.rotation.y = 0;
-	arm_1.transform.rotation.z = 0;
-	arm_1.transform.rotation.w = sqrt(1 - pow(cos(arm_1.transform.rotation.x),2));
+	arm_1.transform.rotation.x = arm_1_quat.x();
+	arm_1.transform.rotation.y = arm_1_quat.y();
+	arm_1.transform.rotation.z = arm_1_quat.z();
+	arm_1.transform.rotation.w = arm_1_quat.w();
 
+	tf2::Quaternion arm_2_quat;
+	arm_2_quat.setRPY(0, M_PI/3.0, 0);
 	arm_2.header.stamp = ros::Time::now();
 	arm_2.header.frame_id = "arm_1";
 	arm_2.child_frame_id = "arm_2";
 	arm_2.transform.translation.x = 0;
-	arm_2.transform.translation.y = arm_1_length;
-	arm_2.transform.translation.z = 0;
-	arm_2.transform.rotation.x = -M_PI/4.0;
-	arm_2.transform.rotation.y = 0;
-	arm_2.transform.rotation.z = 0;
-	arm_2.transform.rotation.w = sqrt(1 - pow(cos(arm_1.transform.rotation.x),2));
+	arm_2.transform.translation.y = 0;
+	arm_2.transform.translation.z = arm_1_length;
+	arm_2.transform.rotation.x = arm_2_quat.x();
+	arm_2.transform.rotation.y = arm_2_quat.y();
+	arm_2.transform.rotation.z = arm_2_quat.z();
+	arm_2.transform.rotation.w = arm_2_quat.w();
 
-	arm_3.header.stamp = ros::Time::now();
-	arm_3.header.frame_id = "arm_2";
-	arm_3.child_frame_id = "arm_3";
-	arm_3.transform.translation.x = 0;
-	arm_3.transform.translation.y = arm_2_length;
-	arm_3.transform.translation.z = 0;
-	arm_3.transform.rotation.x = -M_PI/4.0;
-	arm_3.transform.rotation.y = 0;
-	arm_3.transform.rotation.z = 0;
-	arm_3.transform.rotation.w = sqrt(1 - pow(cos(arm_1.transform.rotation.x),2));
-
+	tf2::Quaternion claw_quat;
+	claw_quat.setRPY(0, M_PI/16.0, 0);
 	claw.header.stamp = ros::Time::now();
-	claw.header.frame_id = "arm_3";
+	claw.header.frame_id = "arm_2";
 	claw.child_frame_id = "claw";
 	claw.transform.translation.x = 0;
-	claw.transform.translation.y = arm_3_length;
-	claw.transform.translation.z = 0;
-	claw.transform.rotation.x = -M_PI/4.0;
-	claw.transform.rotation.y = 0;
-	claw.transform.rotation.z = 0;
-	claw.transform.rotation.w = sqrt(1 - pow(cos(arm_1.transform.rotation.x),2));
+	claw.transform.translation.y = 0;
+	claw.transform.translation.z = arm_2_length;
+	claw.transform.rotation.x = claw_quat.x();
+	claw.transform.rotation.y = claw_quat.y();
+	claw.transform.rotation.z = claw_quat.z();
+	claw.transform.rotation.w = claw_quat.w();
 
 
 	while (ros::ok()){
 
 		camera_forward.header.stamp = ros::Time::now();
+		camera_up.header.stamp = ros::Time::now();
+		camera_down.header.stamp = ros::Time::now();
 		arm_base.header.stamp = ros::Time::now();
 		arm_1.header.stamp = ros::Time::now();
 		arm_2.header.stamp = ros::Time::now();
-		arm_3.header.stamp = ros::Time::now();
 		claw.header.stamp = ros::Time::now();
 
 		tf_br.sendTransform(camera_forward);
+		tf_br.sendTransform(camera_up);
+		tf_br.sendTransform(camera_down);
 		tf_br.sendTransform(arm_base);
 		tf_br.sendTransform(arm_1);
 		tf_br.sendTransform(arm_2);
-		tf_br.sendTransform(arm_3);
 		tf_br.sendTransform(claw);
 
 		rate.sleep();
