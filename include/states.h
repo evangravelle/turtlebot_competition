b1fd1c2350f7fa1b99025a5c487f35648a5be8bd
Matthew Bohl <mbohl@ucsd.edu>
2016-02-26 23:17:02 -0800

    Added control states and message support

--- include/states.h
+++ include/states.h
@@ -1,1 +1,23 @@
-#define INIT 0
+#define 	INIT 						0
+#define 	MANUAL						1
+#define 	START						3
+#define 	END							4
+
+#define 	CONFIG						20
+#define 		CONFIG_GOAL_COLOR		21	
+#define 		CONFIG_CEILING			22
+#define 		CONFIG_BALL_COLOR		23
+
+#define 	FIND_GOAL					40
+
+#define 	MOVE_TO_GOAL				50
+
+#define 	FIND_BALL					60
+
+#define 	MOVE_TO_BALL				70
+
+#define 	PICK_UP_BALL				80	
+
+#define 	DROP_BALL					90
+
+#define     DEFAULT_SUB_STATE           100
